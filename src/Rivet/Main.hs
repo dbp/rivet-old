984ddafa340deba934f52ba15bc1f88d9bbd8b3f
Daniel Patterson <dbp@riseup.net>
2014-09-13 14:14:15 -0400

    add basic dependency support via github (with branches as :branch, directories to add to sandbox with +, separated by commas

--- src/Rivet/Main.hs
+++ src/Rivet/Main.hs
@@ -3,10 +3,16 @@ module Main where
 import           Control.Applicative ((<$>))
 import           Control.Monad       (void)
 import           Data.Configurator
+import           Data.Monoid
 import qualified Data.Text           as T
 import           Development.Shake
+import           Prelude             hiding ((++))
 import           System.Directory
 import           System.Process
+
+
+(++) :: Monoid a => a -> a -> a
+(++) = mappend
 
 opts :: ShakeOptions
 opts = shakeOptions { shakeFiles    = ".shake/" }
@@ -19,11 +25,30 @@ exec :: String -> Action ()
 exec c = liftIO $ do putStrLn c
                      void $ system c
 
+
 main :: IO ()
 main = do
   proj <- getProjectName
   conf <- load [Optional "Rivetfile"]
+  deps <- lookupDefault [] conf (T.pack "dependencies")
+  let depDirs = map ((++ ".d") . ("deps/" ++) . T.unpack . head .
+                     T.splitOn (T.pack ":") . head . T.splitOn (T.pack "+")) deps
   shakeArgs opts $ do
+     sequence_ (map (\d ->
+       do let (repo':rest) = T.splitOn (T.pack "+") d
+          let (repo:branchspec) = T.splitOn (T.pack ":") repo'
+          ("deps/" ++ (T.unpack repo) ++ ".d") *> \depdir ->
+            do cmd ("git clone https://github.com/"
+                    ++ (T.unpack repo) ++ " " ++ depdir) :: Action ()
+               case branchspec of
+                 (branch:_) -> cmd (Cwd depdir) ("git checkout " ++ (T.unpack branch))
+                 _ -> return ()
+               case rest of
+                 (subdirs:_) -> mapM_ (\subdir -> cmd ("cabal sandbox add-source " ++ depdir
+                                                        ++ "/" ++ (T.unpack subdir)) :: Action ())
+                                      (T.splitOn (T.pack ",") subdirs)
+                 _ -> cmd ("cabal sandbox add-source " ++ depdir))
+        deps)
      let binary = "./.cabal-sandbox/bin/" ++ proj
      binary *> \_ -> do files <- getDirectoryFiles "" ["src//*.hs", "*.cabal"]
                         liftIO $ print files
@@ -37,3 +62,7 @@ main = do
                         ++ "_devel -U" ++ proj ++ "_user" ++ " -hlocalhost"
                 exec c
      "repl" ~> exec "cabal repl"
+     "cabal.sandbox.config" *> \_ -> cmd "cabal sandbox init"
+     "init" ~> do need ["cabal.sandbox.config"]
+                  liftIO $ createDirectoryIfMissing False "deps"
+                  need depDirs
