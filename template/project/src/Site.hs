7c1a66ccde2759fce1c27645bf651b58bffe416e
Daniel Patterson <dbp@riseup.net>
2014-09-27 21:19:01 -0400

    move splices out of template Site.hs and into snap-plus; fix version bounds so that template builds

--- template/project/src/Site.hs
+++ template/project/src/Site.hs
@@ -10,12 +10,6 @@ import           Control.Monad.State
 import           Data.ByteString                             (ByteString)
 import qualified Data.Configurator                           as C
 import           Data.Monoid
-import qualified Data.Text                                   as T
-import qualified Data.Text.Encoding                          as T
-import           Heist
-import qualified Heist.Interpreted                           as I
-import           Heist.Splices.BindStrict
-import           Heist.Splices.Ignore
 import           Network.DNS.Resolver
 import           Prelude                                     hiding ((++))
 import           Snap.Snaplet.Heist
@@ -23,10 +17,10 @@ import           Snap.Snaplet.PostgresqlSimple.Plus
 import           Snap.Snaplet.RedisDB
 import           Snap.Snaplet.Session.Backends.CookieSession
 import           Snap.Util.FileServe
-import qualified Text.XmlHtml                                as X
 
 import           Application
 import           Snap.Plus
+import           Snap.Plus.Splices
 
 routes :: [(ByteString, AppHandler ())]
 routes = [ ("",       heistServe)
@@ -40,8 +34,7 @@ app :: SnapletInit App App
 app = makeSnaplet "app" "" Nothing $ do
     h <- nestSnaplet "" heist $ heistInit' "templates"
          mempty { hcLoadTimeSplices = defaultLoadTimeSplices,
-                  hcInterpretedSplices = (do "currentPath" ## pathSplice
-                                             siteSplices) }
+                  hcInterpretedSplices = plusSplices }
     conf <- getSnapletUserConfig
     url <- liftIO (C.require conf "siteUrl")
     absPath <- liftIO (C.lookupDefault "" conf "absolutePath")
@@ -53,28 +46,3 @@ app = makeSnaplet "app" "" Nothing $ do
     e <- getEnvironment
     addRoutes routes
     return $ App h s d r ns url (T.pack e)
-
-prefixUrlSplice :: I.Splice AppHandler
-prefixUrlSplice = do node <- getParamNode
-                     case X.getAttribute "url" node of
-                       Nothing -> return []
-                       Just u -> lift $ ifIsUrl u (return $ X.elementChildren node) (return [])
-
-suffixUrlSplice :: I.Splice AppHandler
-suffixUrlSplice = do node <- getParamNode
-                     case X.getAttribute "url" node of
-                       Nothing -> return []
-                       Just u -> do url <- fmap (T.takeWhile (/= '?') . T.decodeUtf8 . rqURI) getRequest
-                                    return $ if u `T.isSuffixOf` url
-                                              then X.elementChildren node
-                                              else []
-
-pathSplice :: I.Splice AppHandler
-pathSplice = do path' <- getCurrentPath
-                return [X.TextNode path']
-
-siteSplices :: Splices (I.Splice AppHandler)
-siteSplices = do "prefix-url" ## prefixUrlSplice
-                 "suffix-url" ## suffixUrlSplice
-                 bindStrictTag ## bindStrictImpl
-                 ignoreTag ## ignoreImpl
