0ffaab72994a666febd1c619ce08c74b0ce26261
Daniel Patterson <dbp@dbpmail.net>
2014-10-20 22:50:02 -0400

    don't add redundant dependencies to add-all script, and create delete-all script

--- rivet-core/src/Rivet/Rules.hs
+++ rivet-core/src/Rivet/Rules.hs
@@ -1,9 +1,11 @@
 module Rivet.Rules where
 
+import           Prelude           hiding ((++))
+
 import           Control.Monad     (void)
+import           Data.List         (isInfixOf)
 import qualified Data.Text         as T
 import           Development.Shake
-import           Prelude           hiding ((++))
 
 import           Rivet.Common
 
@@ -32,9 +34,16 @@ addDependencies deps =
             case branchspec of
               (branch:_) -> cmd (Cwd depdir) ("git checkout " ++ (T.unpack branch))
               _ -> return ()
-            let addSource s = do let str = "cabal sandbox add-source " ++ s
-                                 liftIO $ appendFile "deps/add-all" (str ++ "\n")
-                                 cmd str :: Action ()
+            let addSource s = do contents <- readFileOrBlank "deps/add-all"
+                                 let addstr = "cabal sandbox add-source " ++ s
+                                 if addstr `isInfixOf` contents
+                                    then return ()
+                                    else liftIO $ appendFile "deps/add-all" (addstr ++ "\n")
+                                 let remstr = "cabal sandbox delete-source " ++ s
+                                 if remstr `isInfixOf` contents
+                                    then return ()
+                                    else liftIO $ appendFile "deps/delete-all" (remstr ++ "\n")
+                                 cmd addstr :: Action ()
             case rest of
               (subdirs:_) -> mapM_ (\subdir -> addSource (depdir ++ "/" ++ (T.unpack subdir)))
                                    (T.splitOn (T.pack ",") subdirs)
@@ -45,3 +54,7 @@ addDependencies deps =
           do need ["deps/dbp/migrate.d/.rivetclone"]
              () <- cmd (Cwd "deps/dbp/migrate.d") "cabal sandbox init"
              cmd (Cwd "deps/dbp/migrate.d") "cabal install"
+  where readFileOrBlank nm = do e <- doesFileExist nm
+                                if e
+                                   then liftIO $ readFile nm
+                                   else return ""
