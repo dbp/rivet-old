7483e505b1db576326a882da85f7b0399a35bdb2
Daniel Patterson <dbp@riseup.net>
2014-10-29 18:23:22 -0400

    fix rivet-docker commands to use script that assumes it'll have the command to run in the CMD env variable

--- rivet-docker/src/Rivet/Lib/Docker.hs
+++ rivet-docker/src/Rivet/Lib/Docker.hs
@@ -18,25 +18,25 @@ runDocker proj _ _ =
   do exec "ln -sf docker/Dockerfile.development Dockerfile"
      exec $ "sudo docker build -t " ++ proj ++ "_devel ."
      exec "rm Dockerfile"
-     void $ exec $ "sudo docker run -w /srv -p 8000:8000 -i -t -v $PWD/docker/data:/var/lib/postgresql -v $PWD/snaplets:/srv/snaplets -v $PWD/templates:/srv/templates -v $PWD/emails:/srv/emails -v $PWD/static:/srv/static -v $PWD/src:/srv/src -v $PWD/devel.cfg:/srv/devel.cfg -v $PWD/defaults.cfg:/srv/defaults.cfg " ++ proj ++ "_devel"
+     void $ exec $ "sudo docker run -w /srv -p 8000:8000 -i -t -v $PWD/docker/data:/var/lib/postgresql -v $PWD/snaplets:/srv/snaplets -v $PWD/templates:/srv/templates -v $PWD/emails:/srv/emails -v $PWD/static:/srv/static -v $PWD/src:/srv/src -v $PWD/devel.cfg:/srv/devel.cfg -v $PWD/defaults.cfg:/srv/defaults.cfg -e CMD=\"rivet run\" " ++ proj ++ "_devel /srv/run"
 
 dbMigrateDocker proj _ _ =
   do exec "ln -sf docker/Dockerfile.development Dockerfile"
      exec $ "sudo docker build -t " ++ proj ++ "_devel ."
      exec "rm Dockerfile"
-     void $ exec $ "sudo docker run -w /srv -i -t -v $PWD/docker/data:/var/lib/postgresql "
-                   ++ proj ++ "_devel rivet db:migrate"
+     void $ exec $ "sudo docker run -w /srv -i -t -v $PWD/migrations:/srv/migrations -v $PWD/docker/data:/var/lib/postgresql -e CMD=\"rivet db:status\" "
+                   ++ proj ++ "_devel /srv/run"
 
 dbMigrateDownDocker proj _ _ =
   do exec "ln -sf docker/Dockerfile.development Dockerfile"
      exec $ "sudo docker build -t " ++ proj ++ "_devel ."
      exec "rm Dockerfile"
-     void $ exec $ "sudo docker run -w /srv -i -t -v $PWD/docker/data:/var/lib/postgresql "
-                   ++ proj ++ "_devel rivet db:migrate:down"
+     void $ exec $ "sudo docker run -w /srv -i -t -v $PWD/migrations:/srv/migrations -v $PWD/docker/data:/var/lib/postgresql -e CMD=\"rivet db:status\" "
+                   ++ proj ++ "_devel /srv/run"
 
 dbStatusDocker proj _ _ =
   do exec "ln -sf docker/Dockerfile.development Dockerfile"
      exec $ "sudo docker build -t " ++ proj ++ "_devel ."
      exec "rm Dockerfile"
-     void $ exec $ "sudo docker run -w /srv -i -t -v $PWD/docker/data:/var/lib/postgresql "
-                   ++ proj ++ "_devel rivet db:status"
+     void $ exec $ "sudo docker run -w /srv -i -t -v $PWD/migrations:/srv/migrations -v $PWD/docker/data:/var/lib/postgresql -e CMD=\"rivet db:status\" "
+                   ++ proj ++ "_devel /srv/run"
