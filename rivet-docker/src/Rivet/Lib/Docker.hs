3d84057b39dd88560562da56710754163a144ac3
Daniel Patterson <dbp@riseup.net>
2014-10-29 22:14:14 -0400

    don't use sudo for docker commands

--- rivet-docker/src/Rivet/Lib/Docker.hs
+++ rivet-docker/src/Rivet/Lib/Docker.hs
@@ -16,18 +16,18 @@ tasks = [Task "run:docker" 0 runDocker ""
 
 cmdDocker proj command =
   do exec "ln -sf docker/Dockerfile.development Dockerfile"
-     exec $ "sudo docker build -t " ++ proj ++ "_devel ."
+     exec $ "docker build -t " ++ proj ++ "_devel ."
      exec "rm Dockerfile"
-     void $ exec $ "sudo docker run -w /" ++ proj ++ " -i -t -v $PWD/migrations:/" ++ proj ++ "/migrations -v $PWD/docker/data:/var/lib/postgresql -e CMD=\"" ++ command ++ "\" "
+     void $ exec $ "docker run -w /" ++ proj ++ " -i -t -v $PWD/migrations:/" ++ proj ++ "/migrations -v $PWD/docker/data:/var/lib/postgresql -e CMD=\"" ++ command ++ "\" "
                    ++ proj ++ "_devel /" ++ proj ++ "/run"
 
 -- NOTE(dbp 2014-10-29): Have directories we want to bind be in
 -- Rivetfile... In particular, templates and emails are not general...
 runDocker proj _ _ =
   do exec "ln -sf docker/Dockerfile.development Dockerfile"
-     exec $ "sudo docker build -t " ++ proj ++ "_devel ."
+     exec $ "docker build -t " ++ proj ++ "_devel ."
      exec "rm Dockerfile"
-     void $ exec $ "sudo docker run -w /" ++ proj ++ " -p 8000:8000 -i -t -v $PWD/docker/data:/var/lib/postgresql -v $PWD/migrations:/" ++ proj ++ "/migrations -v $PWD/snaplets:/" ++ proj ++ "/snaplets -v $PWD/templates:/" ++ proj ++ "/templates -v $PWD/emails:/" ++ proj ++ "/emails -v $PWD/static:/" ++ proj ++ "/static -v $PWD/src:/" ++ proj ++ "/src -v $PWD/devel.cfg:/" ++ proj ++ "/devel.cfg -v $PWD/defaults.cfg:/" ++ proj ++ "/defaults.cfg -e CMD=\"rivet run\" " ++ proj ++ "_devel /" ++ proj ++ "/run"
+     void $ exec $ "docker run -w /" ++ proj ++ " -p 8000:8000 -i -t -v $PWD/docker/data:/var/lib/postgresql -v $PWD/migrations:/" ++ proj ++ "/migrations -v $PWD/snaplets:/" ++ proj ++ "/snaplets -v $PWD/templates:/" ++ proj ++ "/templates -v $PWD/emails:/" ++ proj ++ "/emails -v $PWD/static:/" ++ proj ++ "/static -v $PWD/src:/" ++ proj ++ "/src -v $PWD/devel.cfg:/" ++ proj ++ "/devel.cfg -v $PWD/defaults.cfg:/" ++ proj ++ "/defaults.cfg -e CMD=\"rivet run\" " ++ proj ++ "_devel /" ++ proj ++ "/run"
 
 dbDocker proj _ _ = cmdDocker proj "rivet db"
 
